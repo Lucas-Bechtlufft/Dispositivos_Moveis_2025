51bd30649ffeb491b40575cd39b90c874adc176a
Lucas Bechtlufft <luks_dsk@hotmail.com>
2025-07-08 20:29:31 -0300

    Adicionando no git.

---
+++ app/src/main/java/com/example/sensores/Pipoca.java
@@ -0,0 +1,19 @@
+package com.example.sensores;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+public class Pipoca implements SensorEventListener
+{
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
